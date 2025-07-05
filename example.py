cab29d3bceabe4a147d7673389b2435571ead063
Wang Ziyi <15877161799@163.com>
2025-07-05 20:25:31 +0800

    Create example.py

---
+++ example.py
@@ -0,0 +1,5 @@
+from youdao_translate import *
+
+print(translate("World is a loop."))
+
+# {"code":0,"dictResult":{},"translateResult":[[{"tgt":"世界是一个循环","src":"World is a loop","tgtPronounce":"shì jiè shì yí gè xún huán"}]],"type":"en2zh-CHS"}
